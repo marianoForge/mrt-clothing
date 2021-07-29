72b59c3106b5482d034cada0429107f3e6aa36ef
Mariano De Simone <marianods@MSI-MDS.localdomain>
2021-07-28 21:03:59 -0300

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-
-import './App.css';
+import "./App.css";
+import React from "react";
+import Homepage from "./homepage.component.jsx";
 
 function App() {
   return (
     <div>
-    Mariano
+      <Homepage />
     </div>
   );
 }
